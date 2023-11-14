bb54fe24e8e2dc7690e41b99410d78bd40387aef
jc <jc@emial.com>
2023-11-14 18:42:48 +0800

    feat: add new demo for rainbowkit 1.x version

---
+++ nextjs-rainbowkit/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
