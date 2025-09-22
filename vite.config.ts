74c43716a784ad8fc582db0eafd604401860ba14
kun <k@github-user.com>
2025-09-22 13:35:25 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
+  base: '/woofi-swap-widget-demo/',
   plugins: [
     react(),
     nodePolyfills(), // woofi-swap-widget-kit needs Buffer (from node's buffer module)
