47fb981496a40c4b302a23af73aadae7a17a5e42
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-09-22 12:02:22 +0000

    deploy: 41b2f31297374f76c87c9ac12feb25770ffd91ab

--- assets/arrow-top-CIUXtayI.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-D3gig2F7.js";import"./index-DJ67PX84.js";import"./index.es-BX0gj5LZ.js";const i=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M7 13.99a1 1 0 0 1-1-1V5.4L3.54 7.86a1 1 0 0 1-1.42-1.41L6.3 2.28a1 1 0 0 1 1.41 0l4.17 4.17a1 1 0 1 1-1.41 1.41L8 5.4v7.59a1 1 0 0 1-1 1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as arrowTopSvg};
