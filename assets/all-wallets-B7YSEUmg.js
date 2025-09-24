fb2055685ed105a8fa3c41ec3ef78f0d511c0637
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-09-24 04:22:14 +0000

    deploy: 5025001245328aa7056b24d68d9d74d24a339e1c

--- assets/all-wallets-B7YSEUmg.js
+++
@@ -1,6 +0,0 @@
-import{b as l}from"./core-BhugSoqH.js";import"./index-CYNw4PuH.js";import"./index.es-DlyiC2eQ.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};
