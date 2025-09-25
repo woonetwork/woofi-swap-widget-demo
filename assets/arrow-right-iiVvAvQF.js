191ac7635080129fa240fe716991e27ac9facb7d
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-09-25 12:02:26 +0000

    deploy: c1a7aa4eff541c233a84d9464dbee85fb75066b0

--- assets/arrow-right-iiVvAvQF.js
+++
@@ -1,8 +0,0 @@
-import{b as l}from"./core-Bsow05Hp.js";import"./index-C6eAioPc.js";import"./index.es-DHafRlQd.js";const i=l`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as arrowRightSvg};
