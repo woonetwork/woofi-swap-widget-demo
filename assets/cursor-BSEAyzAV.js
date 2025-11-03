388035c2c5eb29cf14977450e4d3bb3a321e900f
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-11-03 05:43:30 +0000

    deploy: 2e4f180296d8cd8e540848cb3e99522bfdee3d77

---
+++ assets/cursor-BSEAyzAV.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-DBP3QJmT.js";import"./index-Dm3ZLoUp.js";import"./index.es-C1xm-RkW.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
