ba560ec892cb7b341426df67be50fab6162da8ad
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-11-03 11:07:17 +0000

    deploy: fadae3516b91c52b5882d45a8c8e4de38aa22834

--- assets/all-wallets-C33NBL0W.js
+++
@@ -1,6 +0,0 @@
-import{b as l}from"./core-DBP3QJmT.js";import"./index-Dm3ZLoUp.js";import"./index.es-C1xm-RkW.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};
