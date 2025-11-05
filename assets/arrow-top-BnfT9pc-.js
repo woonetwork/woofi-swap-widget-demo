903e6d0a23cab8b237eb46504e79be4b805e3da6
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-11-05 11:47:00 +0000

    deploy: c74c49b4cd4dc08201364adb1690f6f00fcd6f69

--- assets/arrow-top-BnfT9pc-.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-Be0PuJhW.js";import"./index-BFwOE_Yr.js";import"./index.es-DXZZrtq9.js";const i=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M7 13.99a1 1 0 0 1-1-1V5.4L3.54 7.86a1 1 0 0 1-1.42-1.41L6.3 2.28a1 1 0 0 1 1.41 0l4.17 4.17a1 1 0 1 1-1.41 1.41L8 5.4v7.59a1 1 0 0 1-1 1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as arrowTopSvg};
