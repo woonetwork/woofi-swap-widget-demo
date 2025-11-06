9cb0461d5b50b1d4b192b9963c350c0958fa45bb
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-11-06 06:00:56 +0000

    deploy: 361ad75b3c11e1ebd06214b822d0a9e92c57fa63

--- assets/cursor-CgqmygCD.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-S_kDWyXk.js";import"./index-Dh8FPfkb.js";import"./index.es-oOy25KSt.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
