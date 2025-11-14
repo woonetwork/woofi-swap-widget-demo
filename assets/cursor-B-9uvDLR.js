0574c3769ccf74e83415fa9cfe9d98045ffd6d93
riceeeeeeeeee <riceeeeeeeeee@users.noreply.github.com>
2025-11-14 07:43:29 +0000

    deploy: f9b4a61f86f5fa17d25ef404fd52ec926604e3d9

--- assets/cursor-B-9uvDLR.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-hOk89bhu.js";import"./index-_rUm3RNF.js";import"./index.es-BNsVXvso.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
